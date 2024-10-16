d8f35dc7a8804df02c3ad8fa689d108e1a24c749
Fabien Airaud <fabien.airaud@etu.unice.fr>
2024-10-16 15:01:48 +0200

    Add doctor actions

--- web-interface/src/pages/DoctorListPage.tsx
+++ web-interface/src/pages/DoctorListPage.tsx
@@ -1,6 +1,8 @@
-import {TableCell, TableRow} from "@mui/material";
+import {Delete, Edit, Visibility} from "@mui/icons-material";
+import {IconButton, Stack, TableCell, TableRow} from "@mui/material";
 import {useEffect, useState} from "react";
-import {generatePath} from "react-router-dom";
+import {generatePath, useNavigate} from "react-router-dom";
+import {toast} from "react-toastify";
 import ListPageGeneric from "../components/generics/ListPageGeneric.tsx";
 import {TableHeadCell} from "../interfaces/Generics.ts";
 import {Doctor, UserRole} from "../interfaces/User.ts";
@@ -8,11 +10,16 @@ import {getUsersWithRole} from "../services/UserService.ts";
 import {privateRoutes} from "../utils/Routes.ts";
 
 export default function DoctorListPage() {
+    const navigate = useNavigate();
     const [doctors, setDoctors] = useState<Doctor[]>([]);
 
     useEffect(() => {
         setDoctors(getUsersWithRole(UserRole.DOCTOR) as Doctor[]);
     }, []);
+
+    const handleView = () => navigate(generatePath(privateRoutes.doctors.base, {"*": privateRoutes.doctors.view}));
+    const handleEdit = () => navigate(generatePath(privateRoutes.doctors.base, {"*": privateRoutes.doctors.edit}));
+    const handleDelete = (id: number) => toast.success(`Doctor ${id} deleted`);
 
     const tableHeadCells: TableHeadCell[] = [
         {
@@ -48,7 +55,11 @@ export default function DoctorListPage() {
             <TableCell>{row.email}</TableCell>
             <TableCell>{row.phone}</TableCell>
             <TableCell>{row.specialty}</TableCell>
-            <TableCell>Edit / Delete</TableCell>
+            <TableCell><Stack direction={"row"} spacing={1}>
+                <IconButton color={"success"} onClick={handleView}><Visibility/></IconButton>
+                <IconButton color={"info"} onClick={handleEdit}><Edit/></IconButton>
+                <IconButton color={"error"} onClick={() => handleDelete(row.id)}><Delete/></IconButton>
+            </Stack></TableCell>
         </TableRow>
     );
 
