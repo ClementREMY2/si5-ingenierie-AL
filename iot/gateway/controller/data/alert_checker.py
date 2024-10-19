a2debffa6ea65b13311cccb97bbc82d7a06cbb31
Alexis Malosse <alexis.malosse@etu.unice.fr>
2024-10-19 15:35:33 +0200

    fix: sensor types incorrect in gateway alert #23

--- iot/gateway/controller/data/alert_checker.py
+++ iot/gateway/controller/data/alert_checker.py
@@ -2,20 +2,25 @@ import sys
 
 # Map for each sensor the min and max normal values
 SENSOR_RANGES = {
-    'heart-rate': (60, 90),
-    'pulse-oximeter': (90, 100),
-    'glucose': (70, 100),
-    'blood-pressure': (60, 120)
+    'heart_rate': (60, 90),
+    'pulse_oximeter': (90, 100),
+    'glycemia': (70, 100),
+    'blood_pressure': (60, 120)
 }
 
 def check_alert(data):
-    min_value, max_value = SENSOR_RANGES.get(data['type'], (0, 0))
-    type = data['type']
+    # Check if data.type is in SENSOR_RANGES
+    if data['type'] not in SENSOR_RANGES:
+        print(f"[ALERT] Data type {data['type']} is not in the acceptable types")
+        return None
+
+    min_value, max_value = SENSOR_RANGES.get(data['type'])
+    sensor_type = data['type']
     value = data['value']
     if value < min_value:
-        return f'Alert: {type} value {value} is too low'
+        return f'Alert: {sensor_type} value {value} is too low'
     if value > max_value:
-        return f'Alert: {type} value {value} is too high'
+        return f'Alert: {sensor_type} value {value} is too high'
     return None
 
 def send_alert(alert):
