f80ade07425b6f07a2251ca550cac668c1dadeed
Arnaud Avocat Gros <arnaud.avocatgros18@gmail.com>
2024-10-23 22:56:14 +0200

    feat(back): create routes with orm for users service #9

--- back/services/users-service/controllers/doctorsController.js
+++ back/services/users-service/controllers/doctorsController.js
@@ -1,14 +1,14 @@
-const doctorsService = require('../services/doctorsService');   
+const doctorsService = require("../services/doctorsService");
 
 exports.getDoctorById = async (req, res) => {
-    try {
-      const doctor = await doctorsService.getDoctorById(req.params.id);
-      res.json(doctor);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Erreur du serveur');
-    }
+  try {
+    const doctor = await doctorsService.getDoctorById(req.params.id);
+    res.json(doctor);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Erreur du serveur");
   }
+};
 
 exports.getAllDoctors = async (req, res) => {
   try {
@@ -16,6 +16,29 @@ exports.getAllDoctors = async (req, res) => {
     res.json(doctors);
   } catch (err) {
     console.error(err);
-    res.status(500).send('Erreur du serveur');
+    res.status(500).send("Erreur du serveur");
   }
-}
+};
+
+exports.putDoctorById = async (req, res) => {
+  try {
+    const updatedDoctor = await doctorsService.putDoctorId(
+      req.params.id,
+      req.body
+    );
+    res.status(200).json(updatedDoctor);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Erreur du serveur");
+  }
+};
+
+exports.getDoctorPatients = async (req, res) => {
+  try {
+    const patients = await doctorsService.getDoctorPatients(req.params.id);
+    res.status(200).json(patients);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Erreur du serveur");
+  }
+};
