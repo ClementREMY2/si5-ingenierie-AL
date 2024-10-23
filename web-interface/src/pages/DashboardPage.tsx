ed8d5e45c766940bad13a351c6c061bcce0390f1
Samuel <samuel.bois@etu.univ-cotedazur.fr>
2024-10-23 07:19:32 +0200

    #34 : update Dashboard page to display grafana dashboard

--- web-interface/src/pages/DashboardPage.tsx
+++ web-interface/src/pages/DashboardPage.tsx
@@ -1,7 +1,22 @@
 import {Typography} from "@mui/material";
+import React from 'react';
 
-export default function DashboardPage() {
+const DashboardPage = () => {
+    // TODO : update with correct ID
+    const grafanaUrl = "http://localhost:3000/d/your_dashboard_id?orgId=1";
+  
     return (
-        <Typography variant={"h1"}>Dashboard Page</Typography>
+      <div style={{ width: '100%', height: '600px' }}>
+        <iframe 
+          src={grafanaUrl}
+          width="100%"
+          height="100%"
+          frameBorder="0"
+          style={{ overflow: 'hidden' }}
+          title="Grafana Dashboard"
+        />
+      </div>
     );
-}
+  };
+  
+  export default DashboardPage;
